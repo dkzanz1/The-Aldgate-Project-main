bd47cfbe25659b4cde2edad798e8dd9d101cba87
paulzolik <paul.zolik@sky.com>
2024-03-03 11:19:53 +0000

    added real text to ipsum

--- src/components/Aboutussection.js
+++ src/components/Aboutussection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Row,Col,Image } from 'react-bootstrap';
-import TextMaker from '../components/lorem-ipsum';
+// import TextMaker from '../components/lorem-ipsum';
 import '../styles/About.css';
 
 
@@ -8,7 +8,7 @@ function AboutUs() {
     return<>
 
           <Row >
-              <Col>
+              <Col xs={12} md={12} >
                 <h2 className="text-center">About Us</h2>
                 <div className=" position-relative text-center text-background">
                     <Image src="../images/building-in-london.jpeg" 
@@ -18,9 +18,15 @@ function AboutUs() {
                     width="100%" 
                     />
                     <div className="position-absolute top-50 start-50 translate-middle" style = {{ color: 'white' }} >
-                      <div className="text-background">
+                      <div xs={12} md={12} className="text-center text-background">
                         <h3>What we do</h3>
-                        <TextMaker />
+                        <p>We specialize in professional high-riser sprinkler installation,
+                         ensuring top-notch security and compliance. Our service encompasses meticulous planning,
+                          thoughtful design, and seamless installation. From comprehensive planning to tailored design,
+                           we leave no detail overlooked.
+                         Our designers blend functionality with aesthetics, crafting solutions that integrate seamlessly into your vision. With precision and proficiency, our technicians bring designs to life with meticulous installation. We are more than installers; we are partners in your safety journey. Committed to quality and innovation,
+                          we stand as pioneers in high-rise safety. Join us as we redefine skyline safety, one installation at a time.
+                           Let's build a future where safety knows no bounds.</p>
                       </div>
                     </div>
                 </div>
--- src/components/DoubleSection.jsx
+++ src/components/DoubleSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col  } from 'react-bootstrap';
-import TextMaker from "./lorem-ipsum";
+// import TextMaker from "./lorem-ipsum";
 import "../styles/doublesection.css";
 import teamImage from "./pexels-photo-1106476.jpeg";
 import designImage from "./pexels-photo-5292195.webp";
@@ -22,7 +22,18 @@ const  DoubleSection = () => {
                         className="App-picture"
                         alt="" />
                     <div className="text-overlay">
-                        <TextMaker />
+                        <p>
+                        As guardians of your building's security,
+                        we embark on a journey of meticulous planning,
+                        where every detail is a piece of the safety puzzle.
+                        Our process begins with a comprehensive assessment,
+                        With a keen eye for detail 
+                        our team crafts tailored solutions that seamlessly integrate into your architectural vision.
+                        From the placement of sprinkler heads to the layout of piping systems,
+                         every element is carefully orchestrated to optimize functionality and aesthetic appeal.
+                        Our designers bring forth a wealth of experience and expertise,  
+                        Through collaborative dialogue and innovative thinking, 
+                        we transform concepts into tangible designs that elevate the safety and appeal of your high-rise structure.</p>
                     </div>
                 </div>
             </Col>
@@ -38,7 +49,13 @@ const  DoubleSection = () => {
                         alt="" />
         
                     <div className="text-overlay">
-                        <TextMaker />
+                       <p>  Our team comprises seasoned professionals who are committed to ensuring the safety and security of high-rise structures.
+
+At the helm, our project manager leads  seamless coordination from planning to installation.
+Their leadership ensures that every project is executed with efficiency and excellence.
+Behind the scenes, our technicians are the backbone of our operation, executing installations with precision and proficiency. Their expertise ensures that every component is seamlessly integrated,
+ delivering maximum protection and peace of mind to our clients.
+Together, our team stands united in our commitment to excellence, innovation, and safety.one project at a time.</p>
                     </div>
                 </div>
             </Col>
@@ -53,7 +70,15 @@ const  DoubleSection = () => {
                         alt="" />
         
                     <div className="text-overlay">
-                        <TextMaker />
+                      <p>Our Services are crafted to fortify your premises against fire hazards.
+                         Our commitment extends beyond mere installation to the ongoing maintenance and servicing of fire systems,
+                          guaranteeing uninterrupted protection when it matters most.
+                           Trust our advisory service to provide insightful guidance on design concepts,
+                            harmonizing safety imperatives with architectural aesthetics seamlessly.
+                             Whether you seek to enhance an existing system or embark on a new project,
+                              our team offers tailored solutions that prioritize your unique needs and aspirations.
+                               Count on us to provide competitive, market-related quotations,
+                                ensuring transparency and affordability every step of the way.</p>
                     </div>
                 </div>
             </Col>
